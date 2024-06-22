bb90ac7ae0648cc3d1a14de1c24fd94048bbebbb
angoc910203 <24a4041417@hvnh.edu.vn>
2024-06-23 01:37:19 +0700

    sữa lỗi home

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,19 +1,30 @@
-$(document).ready(function(){
-    const slidesContainer = $("#slides-container ");
-    const slide = document.querySelector(".slide-crosell ");
-    const prevButton = $("#slide-arrow-prev ");
-    const nextButton = $("#slide-arrow-next ");
-     $("#slide-arrow-prev").click(function(){
-        let slideWidth = $((".slide-crosell "));
-        $("#slides-container ").scrollLeft()
-     });
-    nextButton.addEventListener("click ", () => {
-        const slideWidth = slide.clientWidth;
-        slidesContainer.scrollLeft += slideWidth;
+$(document).ready(function() {
+    var crosell = 0;
+    var $slidesContainer = $("#slides-container");
+    var widthAllCrossell = $slidesContainer.width(); // Corrected selector with '#'
+
+    $("#slide-arrow-prev").click(function() {
+        let slideWidth = $(".slide-crosell").width() * 3;
+        crosell -= slideWidth;
+
+        // Ensure crosell does not go below zero
+        if (crosell < 0) {
+            crosell = 0;
+        }
+
+        $slidesContainer.scrollLeft(crosell);
     });
-    
-    prevButton.addEventListener("click ", () => {
-        const slideWidth = slide.clientWidth;
-        slidesContainer.scrollLeft -= slideWidth;
+
+    $("#slide-arrow-next").click(function() {
+        let slideWidth = $(".slide-crosell").width() * 3;
+        crosell += slideWidth;
+
+        // Ensure crosell does not exceed the maximum scrollable width
+        let maxScroll = $slidesContainer[0].scrollWidth - $slidesContainer.width();
+        if (crosell > maxScroll) {
+            crosell = maxScroll;
+        }
+
+        $slidesContainer.scrollLeft(crosell);
     });
-})
+});
